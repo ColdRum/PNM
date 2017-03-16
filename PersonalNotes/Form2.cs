d5211b1c7cd91590dfdea8177672406cfdf07d9d
Doktortela <volodymyr_grybinyk@ukr.net>
2017-03-16 20:00:19 +0200

    Add files via upload
    f

--- PersonalNotes/Form2.cs
+++ PersonalNotes/Form2.cs
@@ -13,23 +13,19 @@ namespace PersonalNotes
 {
     public partial class Form2 : Form
     {
-        public string f1 { get; set; }
-        public string f4 { get; set; }
-        public string f5 { get; set; }
+        Form1 f1;
+        Form3 f3 = new Form3();
+        Form4 f4 = new Form4();
+        Form5 f5 = new Form5();
 
-        string pathX;
-
-        public Form2(Form1 f1, string s1, Form4 f4, string s4, Form5 f5, string s5)
+        public Form2(Form1 f1, string s1)
         {
             InitializeComponent();
-            this.f1 = s1;
-            this.f4 = s4;
-            this.f5 = s5;
+            this.f1 = f1;
+            this.f3.Hide();
+            this.f4.Hide();
+            this.f5.Hide();
             label2.Text = s1;
-            textBox1.Text = s4;
-            textBox2.Text = s5;
-
-            pathX = "";
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -39,13 +35,11 @@ namespace PersonalNotes
 
         private void калькуляторToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form3 f3 = new Form3();
             f3.Show();
         }
 
         private void категориюToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form4 f4 = new Form4();
             f4.Show();
         }
 
@@ -61,7 +55,6 @@ namespace PersonalNotes
 
         private void категориюToolStripMenuItem2_Click(object sender, EventArgs e)
         {
-            Form4 f4 = new Form4();
             f4.Show();
         }
 
@@ -72,97 +65,12 @@ namespace PersonalNotes
 
         private void темуЗаметкиToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form5 f5 = new Form5();
             f5.Show();
         }
 
         private void темуToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form5 f5 = new Form5();
             f5.Show();
-        }
-
-        private void назадToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (richTextBox1.CanUndo == true)
-            {
-                richTextBox1.Undo();
-                richTextBox1.ClearUndo();
-            }
-        }
-
-        private void вырезатьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (richTextBox1.SelectionLength > 0)
-            {
-                richTextBox1.Cut();
-            }
-        }
-
-        private void копироватьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (richTextBox1.SelectionLength > 0)
-            {
-                richTextBox1.Copy();
-            }
-        }
-
-        private void вставитьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (Clipboard.GetDataObject().GetDataPresent(DataFormats.Text) == true)
-            {
-                richTextBox1.Paste();
-            }
-        }
-
-        private void найтиToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void заменитьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void выбратьВсёToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            richTextBox1.SelectAll();
-        }
-
-        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (pathX != "")
-            {
-                SaveFileDialog sfd = new SaveFileDialog();
-                sfd.Filter = "Apache Open Office (*.rtf)|*.rtf|Notepad (*.txt)|*.txt";
-                if (sfd.ShowDialog() == DialogResult.OK)
-                {
-                    pathX = sfd.FileName;
-                    richTextBox1.SaveFile(pathX);
-                }
-                else
-                {
-                    richTextBox1.SaveFile(pathX);
-                    MessageBox.Show("Saved.");
-                }
-            }
-        }
-
-        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.Filter = "Apache Open Office (*.rtf)|*.rtf|Notepad (*.txt)|*.txt";
-            if (sfd.ShowDialog() == DialogResult.OK)
-            {
-                pathX = sfd.FileName;
-                richTextBox1.SaveFile(pathX);
-            }
-        }
-
-        private void закрытьПриложениеToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
         }
     }
 }
