ac0fcb2e8dd22ad1950477bc219f9e085befe41f
ColdRum <itrum88@gmail.com>
2017-03-16 19:56:16 +0200

    3 (#4)

--- PersonalNotes/Form2.cs
+++ PersonalNotes/Form2.cs
@@ -17,6 +17,8 @@ namespace PersonalNotes
         public string f4 { get; set; }
         public string f5 { get; set; }
 
+        string pathX;
+
         public Form2(Form1 f1, string s1, Form4 f4, string s4, Form5 f5, string s5)
         {
             InitializeComponent();
@@ -26,6 +28,8 @@ namespace PersonalNotes
             label2.Text = s1;
             textBox1.Text = s4;
             textBox2.Text = s5;
+
+            pathX = "";
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -76,6 +80,89 @@ namespace PersonalNotes
         {
             Form5 f5 = new Form5();
             f5.Show();
+        }
+
+        private void назадToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.CanUndo == true)
+            {
+                richTextBox1.Undo();
+                richTextBox1.ClearUndo();
+            }
+        }
+
+        private void вырезатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionLength > 0)
+            {
+                richTextBox1.Cut();
+            }
+        }
+
+        private void копироватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionLength > 0)
+            {
+                richTextBox1.Copy();
+            }
+        }
+
+        private void вставитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.GetDataObject().GetDataPresent(DataFormats.Text) == true)
+            {
+                richTextBox1.Paste();
+            }
+        }
+
+        private void найтиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void заменитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void выбратьВсёToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pathX != "")
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Apache Open Office (*.rtf)|*.rtf|Notepad (*.txt)|*.txt";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    pathX = sfd.FileName;
+                    richTextBox1.SaveFile(pathX);
+                }
+                else
+                {
+                    richTextBox1.SaveFile(pathX);
+                    MessageBox.Show("Saved.");
+                }
+            }
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Apache Open Office (*.rtf)|*.rtf|Notepad (*.txt)|*.txt";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                pathX = sfd.FileName;
+                richTextBox1.SaveFile(pathX);
+            }
+        }
+
+        private void закрытьПриложениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
